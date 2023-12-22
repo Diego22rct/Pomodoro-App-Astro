5eb150f2b03ab4d7a13dc32ece7cecbab12ea91a
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2023-12-22 18:50:53 -0500

    Add TaskApp component and update dependencies

---
+++ src/pages/pomodoro/TasksApp.tsx
@@ -0,0 +1,7 @@
+export default function TaskApp() {
+	return (
+		<div className="flex flex-col items-center justify-center w-2/5 py-4">
+			<h1 className="text-4xl font-bold">Task App</h1>
+		</div>
+	);
+}
