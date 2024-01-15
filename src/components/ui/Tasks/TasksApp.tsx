57a24d948b98bd985f8f2e4c9f4c6f8469b731ea
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-01-14 21:53:02 -0500

    Add task list to TasksApp component

--- src/components/ui/Tasks/TasksApp.tsx
+++ src/components/ui/Tasks/TasksApp.tsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
+import { Button } from "../button";
+
 export default function TaskApp() {
+	const [Tasks, setTasks] = useState([
+		{
+			id: 1,
+			title: "Task 1",
+			description: "This is task 1",
+			status: "todo",
+		},
+		{
+			id: 2,
+			title: "Task 2",
+			description: "This is task 2",
+			status: "todo",
+		},
+		{
+			id: 3,
+			title: "Task 3",
+			description: "This is task 3",
+			status: "todo",
+		},
+		{
+			id: 4,
+			title: "Task 4",
+			description: "This is task 4",
+			status: "todo",
+		},
+		{
+			id: 5,
+			title: "Task 5",
+			description: "This is task 5",
+			status: "todo",
+		},
+		{
+			id: 6,
+			title: "Task 6",
+			description: "This is task 6",
+			status: "todo",
+		},
+	]);
 	return (
-		<div className="flex flex-col items-center justify-center w-2/5 py-4">
-			<h1 className="text-4xl font-bold">Tasks App</h1>
-		</div>
+		<>
+		<h1 className="text-3xl font-bold">Taks To-Do</h1>
+		<div id="tasks" className="my-5">
+			{
+				Tasks.map((task) => (
+					<div id={task.id.toString()} className="flex justify-between items-center border-b border-slate-200 py-3 px-2 border-l-4  border-l-transparent">
+						<div className="inline-flex items-center space-x-2">
+							<h1 className="text-2xl font-bold">{task.title}</h1>
+							<p className="text-lg">{task.description}</p>
+							<Button>
+								✅
+							</Button>
+						</div>
+					</div>
+				))
+			}
+			</div>
+		</>
 	);
 }
