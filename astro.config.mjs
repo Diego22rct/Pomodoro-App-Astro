efe7a6dc99562d61cf158d190bcada9bf1d75afe
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-01-14 21:01:55 -0500

    Add million compiler plugin for Vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import million from "million/compiler";
+
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+      plugins: [million.vite({ mode: "react", server: true, auto: true })],
+  },
   integrations: [
     react(),
     tailwind({
